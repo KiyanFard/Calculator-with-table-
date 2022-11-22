2b5708f2b484b96ceb9cb71e017cc99082c681f7
KiyanKhatiri <118734818+KiyanKhatiri@users.noreply.github.com>
2022-11-22 11:52:28 +0330

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,70 @@
+document.getElementById('btn-1').addEventListener('click', function() {
+    let number = document.getElementById('result').value;
+    if(parseInt(number) === 0)
+        document.getElementById('result').value = '1';
+    else
+        document.getElementById('result').value = number + '1';
+});
+document.getElementById('btn-2').addEventListener('click', function() {
+     number = document.getElementById('result').value;
+    if(parseInt(number) === 0)
+        document.getElementById('result').value = '2';
+    else
+        document.getElementById('result').value = number + '2';
+});
+document.getElementById('btn-3').addEventListener('click', function() {
+     number = document.getElementById('result').value;
+    if(parseInt(number) === 0)
+        document.getElementById('result').value = '3';
+    else
+        document.getElementById('result').value = number + '3';
+});
+document.getElementById('btn-4').addEventListener('click', function() {
+     number = document.getElementById('result').value;
+    if(parseInt(number) === 0)
+        document.getElementById('result').value = '4';
+    else
+        document.getElementById('result').value = number + '4';
+});
+document.getElementById('btn-5').addEventListener('click', function() {
+     number = document.getElementById('result').value;
+    if(parseInt(number) === 0)
+        document.getElementById('result').value = '5';
+    else
+        document.getElementById('result').value = number + '5';
+});
+document.getElementById('btn-6').addEventListener('click', function() {
+     number = document.getElementById('result').value;
+    if(parseInt(number) === 0)
+        document.getElementById('result').value = '6';
+    else
+        document.getElementById('result').value = number + '6';
+});
+document.getElementById('btn-7').addEventListener('click', function() {
+     number = document.getElementById('result').value;
+    if(parseInt(number) === 0)
+        document.getElementById('result').value = '7';
+    else
+        document.getElementById('result').value = number + '7';
+});
+document.getElementById('btn-8').addEventListener('click', function() {
+     number = document.getElementById('result').value;
+    if(parseInt(number) === 0)
+        document.getElementById('result').value = '8';
+    else
+        document.getElementById('result').value = number + '8';
+});
+document.getElementById('btn-9').addEventListener('click', function() {
+     number = document.getElementById('result').value;
+    if(parseInt(number) === 0)
+        document.getElementById('result').value = '9';
+    else
+        document.getElementById('result').value = number + '9';
+});
+    document.getElementById('btn-0').addEventListener('click', function() {
+     number = document.getElementById('result').value;
+    if(parseInt(number) === 0)
+        document.getElementById('result').value = '0';
+    else
+        document.getElementById('result').value = number + '0';
+});
